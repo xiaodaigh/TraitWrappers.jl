88a99b6d0aeea35b0ef3e459ac18011ce4844692
evalparse <zhuojia.dai@gmail.com>
2019-11-18 21:24:48 +1100

    Files generated by PkgTemplates

---
+++ src/TraitWrapper.jl
@@ -0,0 +1,5 @@
+module TraitWrapper
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using TraitWrapper
+using Test
+
+@testset "TraitWrapper.jl" begin
+    # Write your own tests here.
+end
